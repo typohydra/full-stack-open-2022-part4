1c985fe82c609a7b8b176a7d1b352d73971cda16
typohydra <74554736+typohydra@users.noreply.github.com>
2022-07-25 12:48:59 +0400

    helper functions and unit tests 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce((acc, curr) => acc + curr.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (!blogs.length) return {}
+  const maxLikesBlog = blogs.reduce((max, curr) => curr.likes > max.likes ? curr : max)
+
+  return {
+    title: maxLikesBlog.title,
+    author: maxLikesBlog.author,
+    likes: maxLikesBlog.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
