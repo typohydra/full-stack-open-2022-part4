95d3a328cf83d5a1da96f611b256f178823fe69b
typohydra <74554736+typohydra@users.noreply.github.com>
2022-07-26 12:29:21 +0400

    blog list tests 4.8

---
+++ tests/test_api_helper.js
@@ -0,0 +1,18 @@
+const initialBlogs = [
+  {
+    title: 'HTML intro',
+    author: 'john smith',
+    url: 'blogs.com/htmlintro',
+    likes: 40
+  },
+  {
+    title: 'JS advanced',
+    author: 'lily brown',
+    url: 'blogs.com/jsadvanced',
+    likes: 102
+  }
+]
+
+module.exports = {
+  initialBlogs
+}
