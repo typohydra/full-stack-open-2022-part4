01f9f6cb04fb3e1070cea3ed938b1f27d9f4fde4
typohydra <74554736+typohydra@users.noreply.github.com>
2022-07-26 12:58:38 +0400

    blog list tests 4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -25,6 +25,13 @@ test('all blogs are returned', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('unique identifier property is named id', async() => {
+  const response = await api.get('/api/blogs')
+  for(const blog of response.body) {
+    expect(blog.id).toBeDefined()
+  }
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
