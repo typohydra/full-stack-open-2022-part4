379d49e7b631113c9b07cba6dfb570b53d3f2719
typohydra <74554736+typohydra@users.noreply.github.com>
2022-07-27 12:31:13 +0400

    bloglist expansion 4.17

--- models/user.js
+++ models/user.js
@@ -10,7 +10,13 @@ const userSchema = new mongoose.Schema({
   passwordHash: {
     type: String,
     required: [true, 'password is required']
-  }
+  },
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ],
 })
 
 userSchema.set('toJSON', {
